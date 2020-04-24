c1b1b067765f30a5f10342aca00dd7639d9269c4
Robin Arellano <robinarehen@gmail.com>
2020-04-24 13:00:25 +0200

    refactor: se implementan los metodos del crud en el [ItemApiController]

--- src/main/java/com/rahdevelopers/api/items/apicontroller/ItemApiController.java
+++ src/main/java/com/rahdevelopers/api/items/apicontroller/ItemApiController.java
@@ -13,8 +13,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -29,18 +33,16 @@ public class ItemApiController {
 	@Autowired
 	@Qualifier("serviceImplRest")
 	private ItemService itemService;
-	
+
 	@Autowired
-	private Environment environment; 
-	
-	@Value("${configuracion.text}")
-	private String configText;
-	
+	private Environment environment;
+
 	@GetMapping("/config-server")
-	public ResponseEntity<Map<String, String>> configServer(@Value("${server.port}") String port) {
+	public ResponseEntity<Map<String, String>> configServer(@Value("${configuracion.text}") String configText,
+			@Value("${server.port}") String port) {
 
 		Map<String, String> responseJson = new HashMap<>();
-		responseJson.put("configText", this.configText);
+		responseJson.put("configText", configText);
 		responseJson.put("port", port);
 
 		List<String> profiles = Arrays.asList(this.environment.getActiveProfiles());
@@ -64,7 +66,22 @@ public class ItemApiController {
 	public ItemDto getItem(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return this.itemService.findById(id, cantidad);
 	}
-	
+
+	@PostMapping
+	public ProductoDto postProducto(@RequestBody ProductoDto productoDto) {
+		return this.itemService.save(productoDto);
+	}
+
+	@PutMapping("/{id}")
+	public ProductoDto putProducto(@RequestBody ProductoDto productoDto, @PathVariable Long id) {
+		return this.itemService.update(productoDto, id);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteProducto(@PathVariable Long id) {
+		this.itemService.delete(id);
+	}
+
 	public ItemDto getItemError(Long id, Integer cantidad) {
 		ProductoDto productoDto = new ProductoDto();
 		productoDto.setID(id);
@@ -72,10 +89,10 @@ public class ItemApiController {
 		productoDto.setPrecio(0L);
 		productoDto.setFechaCreacion(new Date());
 		productoDto.setPort(0);
-		
+
 		return new ItemDto(productoDto, cantidad);
 	}
-	
+
 	public List<ItemDto> getLitarItemsError() {
 
 		ProductoDto productoDto = new ProductoDto();
